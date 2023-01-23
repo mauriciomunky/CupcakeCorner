bb6983a6b244276215c5387f6ace9c9dd327a684
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-01-23 19:16:30 -0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Maurício Costa on 23/01/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

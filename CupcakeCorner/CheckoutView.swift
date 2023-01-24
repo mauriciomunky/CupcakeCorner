f774c55eed78fb0b647cd7cee6fdc3e54eb80bef
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-01-24 16:42:08 -0300

    Project complete

---
+++ CupcakeCorner/CheckoutView.swift
@@ -0,0 +1,61 @@
+//
+//  CheckoutView.swift
+//  CupcakeCorner
+//
+//  Created by Maurício Costa on 24/01/23.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @ObservedObject var order: Order
+    @State private var confirmationMessage = ""
+    @State private var showingConfirmation = false
+    
+    func placeOrder() async {
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("Failed to encode order")
+            return
+        }
+        do {
+            let (data, _) = try await URLSession.shared.upload(for: request, from: encoded)
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            confirmationMessage = "Your order for \(decodedOrder.quantity)x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+            showingConfirmation = true
+        } catch {
+            print("Checkout failed.")
+        }
+    }
+    var body: some View {
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string:"https://hws.dev/im/cupcakes@3x.jpg"), scale: 3) {
+                    image in
+                    image.resizable().scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }.frame(height: 233)
+                Text("Your total is \(order.cost, format: .currency(code: Locale.current.currencyCode ?? "BRL"))").font(.title)
+                Button("Place Order") {
+                    Task {
+                        await placeOrder()
+                    }
+                }.padding()
+            }
+        }.navigationTitle("Check out").navigationBarTitleDisplayMode(.inline).alert("Thank you!", isPresented: $showingConfirmation) {
+            Button("OK") { }
+        } message: {
+            Text(confirmationMessage)
+        }
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        CheckoutView(order: Order())
+    }
+}
